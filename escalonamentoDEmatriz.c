92d590845b9e69da80c290d3bc57d6031e489ba3
JoctaTorres <jocta.torres@gmail.com>
2017-05-29 22:20:17 -0300

    Add files via upload

---
+++ escalonamentoDEmatriz.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int li, co; // linha e coluna
+    S:
+    printf("\n DIGITE A DIMENSAO DA MATRIZ. \n LINHAS :: ");
+    scanf(" %d", &li);
+    printf(" COLUNA :: ");
+    scanf(" %d", &co);
+    if(li<co-1)
+    {
+        printf("\n NUMERO DE EQUACOES INSUFICIENTE. TENTE DE NOVO : ");
+        getch();
+        goto S;
+    }
+
+    system("cls");
+    float matriz[li][co];
+    int il, ic; // ind.Linha e ind.Coluna
+    for(il=0 ; il<li ; il++ ) // lendo a matriz
+    {
+        for(ic=0; ic<co; ic++ )
+        {
+            printf("\n VALOR DO ELEMENTO [%d][%d] = ",il+1, ic+1);
+            scanf(" %f", &matriz[il][ic]);
+        }
+    }
+
+    system("cls");
+    printf("\n A MATRIZ AMPLIADA DO SISTEMA :: \n");
+    for(il=0 ; il<li ; il++ ) // imprimindo a matriz
+    {
+        printf("\n");
+        for(ic=0; ic<co; ic++ )
+        {
+            //matriz[il][ic]=0;
+            printf(" \t%.2f", matriz[il][ic]);
+        }
+
+    }
+    int lo, ld, oc, n;
+    float laux[co];
+    float aux, k;
+    for(ic= 0, il=0; il<li; ic++, ++il) //  loop principal para resolver a matriz
+    {
+
+        if (matriz[il][ic]==0) // caso permuta
+        {
+
+            for(lo=il; lo<li; lo++) //acha na coluna um número não nulo
+            {
+                if(matriz[lo][ic]!=0)
+                    ld=lo;
+            }
+            for(oc=0; oc<co; oc++) //realiza a permuta
+            {
+                laux[oc] = matriz[il][oc];
+                matriz[il][oc] = matriz[ld][oc];
+                matriz[ld][oc] = laux[oc];
+            }
+
+        }
+
+        // definindo o primeiro elemento não nulo da linha como um
+        aux=matriz[il][ic];
+        for(oc=ic; oc<co; oc++)
+        {
+            matriz[il][oc]/=aux;
+        }
+        //zera os demais elementos da coluna
+        for (lo=il+1; lo<li; lo++)
+        {
+            k=-matriz[lo][ic];
+            for(oc=ic; oc<co; oc++) // usando uma linha auxiliar: copia e opera a linha selecionada
+            {
+                laux[oc] = matriz[il][oc];
+                laux[oc]*=k;
+                matriz[lo][oc]+=laux[oc];
+            }
+        }
+    }
+    printf("\n\n A MATRIZ ESCALONADA DO SISTEMA :: \n");
+    for(il=0 ; il<li ; il++ ) // imprimindo a matriz
+    {
+        printf("\n");
+        for(ic=0; ic<co; ic++ )
+        {
+            printf(" \t%.2f", matriz[il][ic]); //matriz[il][ic]=0;
+        }
+    }
+    char r;
+    printf("\n\n DESEJA REPETIR? [S/N] : ");
+    scanf(" %c", &r); r=toupper(r);
+    if(r=='S')
+    {
+        system("cls");
+        goto S;
+    }
+    return 0;
+}
